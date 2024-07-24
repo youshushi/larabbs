95f34515cee6299ca9f60fee0b1bdc7461794d85
vagrant <vagrant@homestead>
2024-07-24 06:23:42 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,4 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
